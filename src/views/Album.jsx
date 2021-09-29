73a4c2da30611255471a7580d3a4a0c4eeae237b
Aaron Ahn <aaron@countable.ca>
2021-09-29 17:29:16 +0900

    coming soon

--- src/views/Album.jsx
+++ src/views/Album.jsx
@@ -17,84 +17,16 @@ const breakpointColumnsObj = {
 
 function Album() {
   return (
-    <div className="p-7 md:p-20">
+    <div className="p-7 md:p-20 h-screen">
       <h1 className="text-yellow-500 mt-10 text-center text-4xl tracking-widest">
         Exhibition
       </h1>
       <p className="text-center text-white text-opacity-70 mt-2 tracking-wider ">
-        Highlights 2021
+        2021
       </p>
-      <Masonry
-        breakpointCols={breakpointColumnsObj}
-        className="my-masonry-grid"
-        columnClassName="my-masonry-grid_column"
-      >
-        <div>
-          <Lightbox>
-            <LazyLoadImage
-              src={vancouverLarge_1}
-              className="rounded-sm max-h-full"
-            />
-          </Lightbox>
-        </div>
-        <div>
-          <LazyLoadImage src={vancouverLarge_2} className="rounded-sm" />
-        </div>
-        <div>
-          <LazyLoadImage src={vancouverLarge_3} className="mt-10 rounded-sm" />
-        </div>
-        <div>
-          <LazyLoadImage src={vancouverLarge_4} className="mt-10 rounded-sm" />
-        </div>
-        <div>
-          <LazyLoadImage src={vancouverLarge_1} className="mt-10 rounded-sm" />
-        </div>
-        <div>
-          <LazyLoadImage src={vancouverLarge_2} className="mt-10 rounded-sm" />
-        </div>
-        <div>
-          <LazyLoadImage src={vancouverLarge_3} className="mt-10 rounded-sm" />
-        </div>
-        <div>
-          <LazyLoadImage src={vancouverLarge_4} className="mt-10 rounded-sm" />
-        </div>
-        <div>
-          <LazyLoadImage src={vancouverLarge_1} className="mt-10 rounded-sm" />
-        </div>
-        <div>
-          <LazyLoadImage src={vancouverLarge_2} className="mt-10 rounded-sm" />
-        </div>
-        <div>
-          <LazyLoadImage src={vancouverLarge_3} className="mt-10 rounded-sm" />
-        </div>
-        <div>
-          <LazyLoadImage src={vancouverLarge_4} className="mt-10 rounded-sm" />
-        </div>
-        <div>
-          <LazyLoadImage src={vancouverLarge_1} className="mt-10 rounded-sm" />
-        </div>
-        <div>
-          <LazyLoadImage src={vancouverLarge_2} className="mt-10 rounded-sm" />
-        </div>
-        <div>
-          <LazyLoadImage src={vancouverLarge_3} className="mt-10 rounded-sm" />
-        </div>
-        <div>
-          <LazyLoadImage src={vancouverLarge_4} className="mt-10 rounded-sm" />
-        </div>
-        <div>
-          <LazyLoadImage src={vancouverLarge_1} className="mt-10 rounded-sm" />
-        </div>
-        <div>
-          <LazyLoadImage src={vancouverLarge_2} className="mt-10 rounded-sm" />
-        </div>
-        <div>
-          <LazyLoadImage src={vancouverLarge_3} className="mt-10 rounded-sm" />
-        </div>
-        <div>
-          <LazyLoadImage src={vancouverLarge_4} className="mt-10 rounded-sm" />
-        </div>
-      </Masonry>
+      <p className="text-white text-center mt-5 font-sans text-opacity-80 tracking-wider">
+        Coming soon.
+      </p>
     </div>
   );
 }
